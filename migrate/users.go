d999437f15f0ad13056a44757b8b7051d7fef6a7
Malcolm Jones <malcolm.jones@nytimes.com>
2019-12-10 11:46:56 -0500

    fix typing

--- migrate/users.go
+++ migrate/users.go
@@ -81,7 +81,7 @@ func MigrateUsers(source, target *sql.DB) error {
 		prepared := fmt.Sprintf(usersInsertQuery, qs...)
 		log.Debugln(fmt.Sprintf("%v", prepared))
 
-		result := interface{}
+		var result interface{}
 
 		if err := meddler.QueryRow(tx, &result, prepared, values...); err != nil {
 			log.WithError(err).Errorln("migration failed")
